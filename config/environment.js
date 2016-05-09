7bb846c185bb1832eea4c5240fa2cf24eeff36fa
Daniel McBryde <mcbryded@gmail.com>
2016-05-09 09:50:46 -0500

    begin styles for index routes

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    apiHost: 'http://localhost:3000',
+    apiHost: 'https://american-adventurers-guild.herokuapp.com/',
 
     modulePrefix: 'american-adventurers-guild',
     environment: environment,
