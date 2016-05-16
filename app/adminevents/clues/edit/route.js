c8e836b4aced86ee38b2764d89a0917a203ede22
Daniel McBryde <mcbryded@gmail.com>
2016-05-16 15:17:59 -0500

    create adminevents branch

--- app/adminevents/clues/edit/route.js
+++ app/adminevents/clues/edit/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.modelFor(`events.details`);
+  model({ id }) {
+    return this.store.findRecord(`clue`, id);
   },
 });
---
+++ app/adminevents/details/controller.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  savePrize(event, formStuff) {
+    const prize = this.store.createRecord(`prize`, formStuff);
+    prize.set(`event`, event);
+
+    prize.save();
+  },
+
+  buyThis() {
+    if (window.confirm(`Are you sure you want to purchase this?`)) {
+      this.model.toggleProperty(`active`);
+
+      this.transitionToRoute(`adminevents.details.clues`, this.model);
+    }
+  },
+});
