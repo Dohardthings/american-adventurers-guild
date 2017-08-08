80c5248118ca77d966d62c0c287ba67d7a586d88
Daniel McBryde <mcbryded@gmail.com>
2017-08-08 13:03:32 -0500

    add more tests to include logintest.

--- testing/test-1.js
+++ testing/test-1.js
@@ -7,4 +7,24 @@ describe(`Are elements visible`, function () {
       expect(browser.isVisible(`#ember508 > button`)).to.be.true;
     });
   });
+
+  describe(`Login`, function () {
+    it(`should log in successfully if credentials are valid`, function () {
+      browser.url(`http://localhost:4200/`);
+      browser.click(`#ember525 > button`);
+      expect(browser.getUrl(`http://localhost:4200/login`));
+
+      // Fill in Input for Name
+      browser.element(`#input-ember648`).setValue(`ihatemyjob4@hotmail.com`);
+      // Fill in Input for Subject
+      browser.element(`#input-ember658`).setValue(`daniel`);
+      // Click on Submit button
+      expect(browser.isVisible(`#login`)).to.be.true;
+      browser.click(`#login`);
+
+      // Check Resulting Url is course-directory
+      browser.waitForVisible(`#events`);
+      expect(browser.isVisible(`#events`)).to.be.true;
+    });
+  });
 });
